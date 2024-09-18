6ae8906cf8e8bf7ea19779bce583f22629777dca
Otiong <oscarloltiong@gmail.com>
2024-09-18 01:08:12 -0400

    The velocity resets each fall

--- IGME690 Looking Glass/Assets/Scripts/ReloadItem.cs
+++ IGME690 Looking Glass/Assets/Scripts/ReloadItem.cs
@@ -6,6 +6,8 @@ public class ReloadItem : MonoBehaviour
 {
     //this is just where the ceiling game object is so i don't place the item above it
     public Transform ceiling;
+    //adjust the amount of time it takes before respawning an elephant
+    public float resetTime = 0.5f;
     //all the bouncy items in order that they will replace each other
     public List<GameObject> itemsToDrop;
 
@@ -23,17 +25,25 @@ public class ReloadItem : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    public IEnumerator ResetItem()
     {
-        //remove last item
         itemsToDrop[currentItem].SetActive(false);
         //iterate to next item
         currentItem++;
         //make sure that the next item actually exists
         if (currentItem > itemsToDrop.Count - 1) { currentItem = 0; }
+        itemsToDrop[currentItem].GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+
+        yield return new WaitForSeconds(resetTime);
         //set the current item to active
         itemsToDrop[currentItem].SetActive(true);
         //place it right below the ceiling
         itemsToDrop[currentItem].GetComponent<Transform>().position = new Vector3(0, ceiling.position.y - 1f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //remove last item
+        StartCoroutine(ResetItem());
     }
 }
